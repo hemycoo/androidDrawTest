07a8c61deae52c7b4b09b90d5f1464c177f2a095
hemycoo <hemycoo_2017@126.com>
2018-10-24 18:24:16 +0800

    提交测试2

--- src/main/java/com/test/drawtest1/MainActivity.kt
+++ src/main/java/com/test/drawtest1/MainActivity.kt
@@ -2,11 +2,13 @@ package com.test.drawtest1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("123","log");
     }
 }
